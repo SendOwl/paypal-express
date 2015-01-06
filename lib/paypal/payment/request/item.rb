4ee394eff9af7f31de00dd265897f75c99379083
George Palmer <george.palmer@gmail.com>
2015-01-06 15:15:15 +0000

    Support tax amount on cart items

--- lib/paypal/payment/request/item.rb
+++ lib/paypal/payment/request/item.rb
@@ -1,7 +1,7 @@
 module Paypal
   module Payment
     class Request::Item < Base
-      attr_optional :name, :description, :amount, :number, :quantity, :category, :url
+      attr_optional :name, :description, :amount, :number, :quantity, :category, :url, :tax_amount
 
       def initialize(attributes = {})
         super
@@ -13,6 +13,7 @@ module Paypal
           :"L_PAYMENTREQUEST_#{parent_index}_NAME#{index}" => self.name,
           :"L_PAYMENTREQUEST_#{parent_index}_DESC#{index}" => self.description,
           :"L_PAYMENTREQUEST_#{parent_index}_AMT#{index}" => Util.formatted_amount(self.amount),
+          :"L_PAYMENTREQUEST_#{parent_index}_TAXAMT#{index}" => Util.formatted_amount(self.tax_amount),
           :"L_PAYMENTREQUEST_#{parent_index}_NUMBER#{index}" => self.number,
           :"L_PAYMENTREQUEST_#{parent_index}_QTY#{index}" => self.quantity,
           :"L_PAYMENTREQUEST_#{parent_index}_ITEMCATEGORY#{index}" => self.category,
